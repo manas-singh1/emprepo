e95717a95b5083ee84604c74d6bc4d9be2d9fca7
Manas Singh <manassingh191999@gmail.com>
2022-07-08 15:18:52 +0530

    Controller Added

---
+++ WebAppAssingment/WebAppAssingment/Data/EmpDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAssingment.Models;
+
+namespace WebAppAssingment.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext (DbContextOptions<EmpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAssingment.Models.Emp> Emp { get; set; }
+    }
+}
---
+++ WebAppAssingment/WebAppAssingment/Models/Emp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppAssingment.Models
+{
+    [Table("Employeer")]
+    public class Emp
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public int EmployeeId { get; set; }
+        public string EDesign { get; set; }
+        public DateTime EDoj { get; set; }
+    }
+}
