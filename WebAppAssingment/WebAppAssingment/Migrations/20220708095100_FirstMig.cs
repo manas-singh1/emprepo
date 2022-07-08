e1528bfb0d309fdec66203f1e10b63deb9c13b6a
Manas Singh <manassingh191999@gmail.com>
2022-07-08 15:32:42 +0530

    Third Commit Database linked

---
+++ WebAppAssingment/WebAppAssingment/Migrations/20220708095100_FirstMig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppAssingment.Migrations
+{
+    public partial class FirstMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employeer",
+                columns: table => new
+                {
+                    EId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    EDesign = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EDoj = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employeer", x => x.EId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employeer");
+        }
+    }
+}
---
+++ WebAppAssingment/WebAppAssingment/obj/Debug/net5.0/l24tntsr.sql
@@ -0,0 +1,30 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Employeer] (
+    [EId] int NOT NULL IDENTITY,
+    [EName] nvarchar(max) NULL,
+    [EmployeeId] int NOT NULL,
+    [EDesign] nvarchar(max) NULL,
+    [EDoj] datetime2 NOT NULL,
+    CONSTRAINT [PK_Employeer] PRIMARY KEY ([EId])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220708095100_FirstMig', N'5.0.17');
+GO
+
+COMMIT;
+GO
+
